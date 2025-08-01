ce6aea23b4bae215ab62eb43f2ff93f19276d38a
Abhishek <abhisheksangule6@gmail.com>
2025-08-01 23:48:12 +0530

    change the config to simiple

--- client/src/conf/config.jsx
+++ client/src/conf/config.jsx
@@ -1,5 +1,3 @@
 // src/config.js
-export const API_BASE =
-    import.meta.env.NODE_ENV === 'production'
-        ? 'https://readme-view-counter.onrender.com/api'
-        : 'http://localhost:4000/api';
+export const API_BASE = 'https://readme-view-counter.onrender.com/api'
+
