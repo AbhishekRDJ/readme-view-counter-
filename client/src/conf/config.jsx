b871318c1539c2145b39220c1d49dfeafeacf882
Abhishek <abhisheksangule6@gmail.com>
2025-08-01 18:50:26 +0530

    added the deployed url to config

--- client/src/conf/config.jsx
+++ client/src/conf/config.jsx
@@ -1,5 +1,5 @@
 // src/config.js
 export const API_BASE =
     import.meta.env.NODE_ENV === 'production'
-        ? 'https://your-backend-domain.com/api'
+        ? 'https://readme-view-counter.onrender.com/api'
         : 'http://localhost:4000/api';
