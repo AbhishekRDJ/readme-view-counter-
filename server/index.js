b714684736d628d9b2b01d7fb0f6e6ff785a38f7
Abhishek <abhisheksangule6@gmail.com>
2025-08-01 18:48:03 +0530

    typo mistake

--- server/index.js
+++ server/index.js
@@ -10,10 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 app.use('/api/counters', counterRoutes);
-app.use('/', (req, res) => {
-    console.log("Server is Running...app.use.");
-
-})
+app.get('/', (req, res) => {
+    res.send('🚀 View Counter API is running...');
+});
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('🗄️ MongoDB connected'))
